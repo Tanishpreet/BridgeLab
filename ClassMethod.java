edfa0a073f6ede6ee88ddf86e8d7c67114fa50a6
Tanishpreet Kaur <kaurtanishpreet320@gmail.com>
2025-06-30 12:02:44 +0530

    adding new java file

---
+++ ClassMethod.java
@@ -0,0 +1,15 @@
+public class ClassMethod{
+int a=2;
+
+    public static void main(String[] args) {
+//int b;
+//b=1;
+ System.out.println("Tanishpreet");
+ int a=1;
+ int b=2;
+ int c = a+b;
+ int d = b-a;
+ System.out.println(c);
+ System.out.println(d);
+    }
+}
